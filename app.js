6d43ab2c9306cb9d3653fa369621202623105b97
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-16 02:58:01 +0900

    add and register Router objects

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
+// Expressで利用する npm モジュールを読み込む
+var createError = require('http-errors');     // HTTPのエラーを作成するモジュール
+var express = require('express');             // Expressの本体
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cookieParser = require('cookie-parser');  // Cookieを解釈するモジュール
+var logger = require('morgan');               // コンソールにログを整形して出力するモジュール
+var helmet = require('helmet');               // Expressの機能を拡張し、HTTPにおいて脆弱性となるヘッダなどを取り除くなどし、安全に使えるようにしてくれる様々な脆弱性に対する機能を備えたモジュール
 
+// routesディレクトリの中にあるRouterオブジェクトのモジュールを読み込む
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var photosRouter = require('./routes/photos');
 
-var app = express();
+var app = express();  // Applicationオブジェクトをexpressのモジュールを利用して作成し、appという変数に格納
+app.use(helmet());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Applicationの設定を行う
+app.set('views', path.join(__dirname, 'views'));  // テンプレートのファイルがviewsディレクトリにあることを設定
+app.set('view engine', 'pug');                    // テンプレートエンジンが pug であることを設定
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,13 +26,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler　存在しないパスへのアクセスがあった際の処理
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler　エラー処理：views/error.pug というテンプレートを使ってエラーを表示させる
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
